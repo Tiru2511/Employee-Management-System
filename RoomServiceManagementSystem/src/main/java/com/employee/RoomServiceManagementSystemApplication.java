3df449f60846b4436bc36d263cfd2fec37011946
Yeddula Shantan <yshantaninfo@gmail.com>
2025-01-25 15:55:47 +0530

    done with the project setup

---
+++ RoomServiceManagementSystem/src/main/java/com/employee/RoomServiceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomServiceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomServiceManagementSystemApplication.class, args);
+	}
+
+}
